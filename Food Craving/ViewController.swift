2bce37eca8ed444016c4c838ef6f4f2888dc89ad
Ross <ross@duris.io>
2016-03-18 20:59:31 -0400

    feat: Get data from Yelp API

--- Food Craving/ViewController.swift
+++ Food Craving/ViewController.swift
@@ -12,14 +12,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        runSearch("Hamburgers")
+        runSearch("Pasta")
+        runSearch("Sushi")
+        runSearch("Wings")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+
+    func runSearch(term: String) {
+        let parameters = [
+            "ll": "39.9797, -83.0047",
+            "category_filter": "food",
+            "radius_filter": "4000",
+            "sort": "0",
+            "term": term,
+            "limit": "5"]
+        
+        YelpClient.sharedInstance().searchPlacesWithParameters(parameters, successSearch: { (data, response) -> Void in
+            //print(NSString(data: data, encoding: NSUTF8StringEncoding))
+            dispatch_async(dispatch_get_main_queue(), {
+                do {
+                    let jsonArray = try NSJSONSerialization.JSONObjectWithData(data, options:[])
+                    
+                    if let businesses = jsonArray["businesses"] as? NSArray{
+                        for biz in (businesses as? [[String:AnyObject]])!{
+                            if let name = biz["name"] {
+                                print(name)
+                                
+                                if let id = biz["id"]{
+                                    //print(id)
+                                }
+                                
+                                let image_url = biz["image_url"]
+                            }
+                        }
+                    }
+                    
+                }
+                    
+                catch {
+                    print("Error: \(error)")
+                }
+            })
+            
+            }, failureSearch: { (error) -> Void in
+                print(error)
+        })
     }
-
-
 }
 
